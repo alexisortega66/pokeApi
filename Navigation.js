14c77a92877337cb0d0cd7954400301b8ddd8cb1
alexisortega66 <mortega_20@alu.uabcs.mx>
2023-11-20 19:41:50 -0700

    se agrego 2 tabs, uno de home y otro solo para la pantalla de buscar

--- Navigation.js
+++ Navigation.js
@@ -1,40 +1,75 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createStackNavigator } from "@react-navigation/stack";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 import HomeScreen from "./views/HomeScreen";
 import DetailsScreen from "./views/DetailsScreen";
 import PokemonsList from "./views/PokemonsList";
+import SearchScreen from "./views/SearchScreen";
 
+const Tab = createBottomTabNavigator();
+const HomeStack = createStackNavigator();
+const SearchStack = createStackNavigator();
 
-const MainNavigator = createStackNavigator();
+const HomeStackScreen = () => (
+  <HomeStack.Navigator>
+    <HomeStack.Screen
+      name="HomeScreen"
+      component={HomeScreen}
+      options={{ headerShown: false }}
+    />
+    <HomeStack.Screen
+      name="DetailsScreen"
+      component={DetailsScreen}
+      options={{ headerShown: false }}
+    />
+    <HomeStack.Screen
+      name="PokemonsList"
+      component={PokemonsList}
+      options={{ headerShown: false }}
+    />
+  </HomeStack.Navigator>
+);
 
+const SearchStackScreen = () => (
+  <SearchStack.Navigator>
+    <SearchStack.Screen
+      name="SearchScreen"
+      component={SearchScreen}
+      options={{ headerShown: false }}
+    />
+  </SearchStack.Navigator>
+);
 
-function MyStack() {
+const MyTabs = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeStackScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" size={24} color="black" />
+          ),
+        }}
+      />
+      <Tab.Screen name="Search" component={SearchStackScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome5 name="search" size={24} color="black" />
+          ),
+        }} />
+    </Tab.Navigator>
+  );
+};
+
+function Navigation() {
   return (
     <NavigationContainer>
-      <MainNavigator.Navigator initialRouteName="HomeScreen">
-        <MainNavigator.Screen name="HomeScreen" component={HomeScreen} 
-        options={{
-            headerShown: false,
-        }
-        }/>
-        <MainNavigator.Screen name="DetailsScreen" component={DetailsScreen} 
-         options={{
-            headerShown: false,
-        }
-        }/>
-    
-    <MainNavigator.Screen name="PokemonsList" component={PokemonsList} 
-         options={{
-            headerShown: false,
-        }
-        }/>
-      </MainNavigator.Navigator>
+      <MyTabs />
     </NavigationContainer>
   );
 }
 
-export default function Navigation() {
-  return <MyStack />;
-}
+export default Navigation;
